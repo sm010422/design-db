9042600c61c182d81bb851a6b9df2c61ce1fbedb
Park Sang Min <sm0104224@gmail.com>
2025-09-13 13:41:36 +0900

    first commit for design Database

---
+++ designdb/src/test/java/com/example/designdb/DesigndbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigndbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
